f1d48be17729e6db9d012872d994c08abc073cca
Dejing Ma <mdj940619@163.com>
2016-08-12 11:18:32 +0800

    Add files via upload

---
+++ mock.js
@@ -0,0 +1,55 @@
+/**
+ * Created by MelanchoJy on 16/8/5.
+ */
+
+export const localSectionArr = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'N', 'S'];
+export const localData = [
+  {cityId: '1', cityName: '北京', cityAleph: 'B'},
+  {cityId: '2', cityName: '贵阳', cityAleph: 'G'},
+  {cityId: '3', cityName: '大连', cityAleph: 'D'},
+  {cityId: '4', cityName: '成都', cityAleph: 'C'},
+  {cityId: '5', cityName: '福州', cityAleph: 'F'},
+  {cityId: '6', cityName: '广州', cityAleph: 'G'},
+  {cityId: '7', cityName: '重庆', cityAleph: 'C'},
+  {cityId: '8', cityName: '哈尔滨', cityAleph: 'H'},
+  {cityId: '9', cityName: '宁波', cityAleph: 'N'},
+  {cityId: '10', cityName: '上海', cityAleph: 'S'},
+  {cityId: '11', cityName: '长春', cityAleph: 'C'},
+  {cityId: '12', cityName: '拉萨', cityAleph: 'L'},
+  {cityId: '13', cityName: '长沙', cityAleph: 'C'},
+  {cityId: '14', cityName: '吉林', cityAleph: 'J'},
+  {cityId: '15', cityName: '昆明', cityAleph: 'K'},
+  {cityId: '16', cityName: '南京', cityAleph: 'N'},
+  {cityId: '17', cityName: '大同', cityAleph: 'D'},
+  {cityId: '18', cityName: '杭州', cityAleph: 'H'},
+  {cityId: '19', cityName: '合肥', cityAleph: 'H'},
+  {cityId: '20', cityName: '海口', cityAleph: 'H'},
+  {cityId: '21', cityName: '济南', cityAleph: 'J'},
+  {cityId: '22', cityName: '石家庄', cityAleph: 'S'},
+  {cityId: '23', cityName: '兰州', cityAleph: 'L'}
+];
+
+export const foreignSectionArr = ['A', 'B', 'D', 'F', 'G', 'H', 'J', 'L', 'M', 'N', 'R', 'S'];
+export const foreignData = [
+  {cityId: '01', cityName: '阿姆斯特丹', cityAleph: 'A'},
+  {cityId: '02', cityName: '巴黎', cityAleph: 'B'},
+  {cityId: '03', cityName: '巴塞罗那', cityAleph: 'B'},
+  {cityId: '04', cityName: '柏林', cityAleph: 'B'},
+  {cityId: '05', cityName: '大阪', cityAleph: 'D'},
+  {cityId: '06', cityName: '东京', cityAleph: 'D'},
+  {cityId: '07', cityName: '法兰克福', cityAleph: 'F'},
+  {cityId: '08', cityName: '哥本哈根', cityAleph: 'G'},
+  {cityId: '09', cityName: '赫尔辛基', cityAleph: 'H'},
+  {cityId: '010', cityName: '华盛顿', cityAleph: 'H'},
+  {cityId: '011', cityName: '旧金山', cityAleph: 'J'},
+  {cityId: '012', cityName: '伦敦', cityAleph: 'L'},
+  {cityId: '013', cityName: '洛杉矶', cityAleph: 'L'},
+  {cityId: '014', cityName: '慕尼黑', cityAleph: 'M'},
+  {cityId: '015', cityName: '曼谷', cityAleph: 'M'},
+  {cityId: '016', cityName: '纽约', cityAleph: 'N'},
+  {cityId: '017', cityName: '仁川', cityAleph: 'R'},
+  {cityId: '018', cityName: '首尔', cityAleph: 'S'}
+];
+export const locationCity = {cityId: '1', cityName: '北京', cityAleph: 'B'};
+
+
